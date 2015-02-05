ad8a3f81a870ed876230568365b2efeafbbacd7f
燕剑波 <yanshiyi1983@163.com>
2015-02-05 21:27:12 +0800

    commit answers

---
+++ src/koans/14_destructuring.clj
@@ -0,0 +1,46 @@
+(ns koans.14-destructuring
+  (:require [koan-engine.core :refer :all]))
+
+(def test-address
+  {:street-address "123 Test Lane"
+   :city "Testerville"
+   :state "TX"})
+
+(meditations
+  "Destructuring is an arbiter: it breaks up arguments"
+  (= ":bar:foo" ((fn [[a b]] (str b a))
+         [:foo :bar]))
+
+  "Whether in function definitions"
+  (= (str "First comes love, "
+          "then comes marriage, "
+          "then comes Clojure with the baby carriage")
+     ((fn [[a b c]] 
+        (format "First comes %s, then comes %s, then comes %s with the baby carriage" a b c))
+      ["love" "marriage" "Clojure"]))
+
+  "Or in let expressions"
+  (= "Rich Hickey aka The Clojurer aka Go Time aka Macro Killah"
+     (let [[first-name last-name & aliases]
+           (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
+       (clojure.string/join " aka " (cons (str first-name " " last-name) aliases))))
+
+     "You can regain the full argument if you like arguing"
+  (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
+     (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
+      {:original-parts full-name :named-parts {:first first-name :last last-name}}))
+
+  "Break up maps by key"
+  (= "123 Test Lane, Testerville, TX"
+     (let [{street-address :street-address, city :city, state :state} test-address]
+       (clojure.string/join ", " [street-address city state])))
+
+  "Or more succinctly"
+  (= "123 Test Lane, Testerville, TX"
+     (let [{:keys [street-address city state ]} test-address]
+       (clojure.string/join ", " [street-address city state])))
+
+  "All together now!"
+  (= "Test Testerson, 123 Test Lane, Testerville, TX"
+     ((fn [[t1 t2] {:keys [street-address city state]}]
+        (clojure.string/join ", " [(str t1 " " t2), street-address city state])) ["Test" "Testerson"] test-address)))
